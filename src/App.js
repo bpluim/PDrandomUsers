ee0b6f1ccb4dbdbc66b23797fa0271002ac4d174
Ben Pluim <benpluim@Bens-MacBook-Pro.local>
2022-10-31 17:38:37 -0600

    transferring files from other repo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from '@emotion/styled';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Home from './pages/Home';
+import Search from './pages/Search';
+import Profile from './pages/Profile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+    <Router>
+      <Nav>
+        <Link to="/">Home</Link>
+        <Link to="/search">Search</Link>
+      </Nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/member/:id" element={<Profile />} />
+        
+      </Routes>
+    </Router>
+);
+
+const Nav = styled.nav({
+  height: '100px',
+  background: '#282c34',
+  padding: '0 20%',
+  display: 'flex',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  '& > a': {
+    color: '#fff',
+    marginRight: 15,
+    textDecoration: 'none',
+    '&:visited': {
+      color: '#fff'
+    }
+  }
+})
 
 export default App;
